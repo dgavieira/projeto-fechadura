935aed83bc6fcffc088d89e1c8fbbe0bea7c0d43
Diego <diegoalcantaravieira@gmail.com>
2019-04-04 16:54:31 -0400

    testes com checkbutton - classe pronta

--- Projeto-Fechadura/Checkbutton Tests/checktest01.py
+++ Projeto-Fechadura/Checkbutton Tests/checktest01.py
@@ -29,6 +29,18 @@ def telaquatro()
             self.terceiroContainer["padx"] = 20
             self.terceiroContainer.pack()
             
+            self.quartoContainer = Frame(master)
+            self.quartoContainer["padx"] = 20
+            self.quartoContainer.pack()
+            
+            self.quintoContainer = Frame(master)
+            self.quintoContainer["padx"] = 20
+            self.quintoContainer.pack()
+            
+            self.sextoContainer = Frame(master)
+            self.sextoContainer["padx"] = 20
+            self.sextoContainer.pack()
+            
             #elemento do primeiro container
             self.titulo = Label(self.primeiroContainer)
             self.titulo["text"] = "ENROLL"
@@ -50,8 +62,64 @@ def telaquatro()
             self.var = IntVar()
             self.check = Checkbutton(self.terceiroContainer)
             self.check["font"] = self.fontePadrao
-            self.check["text"] = "Administrador"
+            self.check["text"] = "Admin"
             self.check["variable"] = self.var
             self.check["onvalue"] = 1
             self.check ["offvalue"] = 0
             self.check.pack(side = LEFT)
+            
+            #elemento do quarto container
+            self.botaoLoad = Button(self.quartoContainer)
+            self.botaoLoad["text"] = "LOAD"
+            self.botaoLoad["font"] = self.fontePadrao
+            self.botaoLoad["command"] = self.showinput
+            self.botaoLoad["width"] = 30
+            self.botaoLoad.pack()
+            
+            #elemento do quinto container
+            self.msg = Message(self.quintoContainer)
+            self.msg["text"] = "Name: \n Admin:"
+            self.msg["relief"] = SUNKEN
+            self.msg.pack(fill = X, expand = YES)
+
+            #elemento do sexto container
+            self.botaoMainMenu = Button(self.sextoContainer)
+            self.botaoMainMenu["text"] = "MAIN MENU"
+            self.botaoMainMenu["font"] = self.fontePadrao
+            #self.botaoMainMenu["command"] ==
+            self.botaoMainMenu["width"] = 10
+            self.botao.pack(side = LEFT)
+            
+            self.botaoCancel = Button(self.sextoContainer)
+            self.botaoCancel["text"] = "CANCEL"
+            self.botaoCancel["font"] = self.fontePadrao
+            self.botaoCancel["command"] = self.eraseinput
+            self.botaoCancel["width"] = 10
+            self.botao.pack(side = LEFT)
+            
+            self.botaoFingerprint = Button(self.sextoContainer)
+            self.botaoFingerprint["text"] = "FINGERPRINT"
+            self.botaoFingerprint["font"] = self.fontePadrao
+            self.botaoFingeprint["command"] = self.enabledb
+            self.botaoFingerprint["width"] = 10
+            self.botao.pack(side = LEFT)
+            
+        def showinput(self):
+            p_name = self.name.get()
+            p_admin = self.var
+            
+            if self.msg["text"] == "Name: \n Admin:" and p_admin == 1:
+                self.msg["text"] = "Name: " + p_name + "\n Admin: YES"
+                self.botaoFingerprint["state"] = DISABLED
+            elif self.msg["text"] == "Name: \n Admin:" and p_admin == 0:
+                self.msg["text"] = "Name: " + p_name + "\n Aadmin: NO"
+                self.botaoFingerprint["state"] = DISABLED
+            else:
+                self.msg["text"] = "Name: \n Admin:"
+                
+        def eraseinput(self);
+            p_name = self.name.get()
+            p_admin = self.var
+            
+            
+                
