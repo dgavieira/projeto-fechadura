beff7e4e8f43d4805554c4308a83ea9193c1f414
Diego <diegoalcantaravieira@gmail.com>
2019-04-05 14:40:57 -0400

    checkbutton test - precisa debugar

--- Projeto-Fechadura/Checkbutton Tests/checktest01.py
+++ Projeto-Fechadura/Checkbutton Tests/checktest01.py
@@ -12,9 +12,9 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS optima (
 conn.commit()
 conn.close()
 
-def telaquatro()
+def telaquatro():
     class ScreenFour:
-        def __init__(self, master = None)
+        def __init__(self, master = None):
             self.fontePadrao = ("Arial", "10")
             
             self.primeiroContainer = Frame(master)
@@ -35,7 +35,7 @@ def telaquatro()
             
             self.quintoContainer = Frame(master)
             self.quintoContainer["padx"] = 20
-            self.quintoContainer.pack()
+            self.quintoContainer.pack(fill = X, expand = YES)
             
             self.sextoContainer = Frame(master)
             self.sextoContainer["padx"] = 20
@@ -64,15 +64,13 @@ def telaquatro()
             self.check["font"] = self.fontePadrao
             self.check["text"] = "Admin"
             self.check["variable"] = self.var
-            self.check["onvalue"] = 1
-            self.check ["offvalue"] = 0
             self.check.pack(side = LEFT)
             
             #elemento do quarto container
             self.botaoLoad = Button(self.quartoContainer)
             self.botaoLoad["text"] = "LOAD"
             self.botaoLoad["font"] = self.fontePadrao
-            self.botaoLoad["command"] = self.showinput
+            #self.botaoLoad["command"] = self.showinput
             self.botaoLoad["width"] = 30
             self.botaoLoad.pack()
             
@@ -86,38 +84,39 @@ def telaquatro()
             self.botaoMainMenu = Button(self.sextoContainer)
             self.botaoMainMenu["text"] = "MAIN MENU"
             self.botaoMainMenu["font"] = self.fontePadrao
-            #self.botaoMainMenu["command"] ==
+            self.botaoMainMenu["command"] = returntohome
             self.botaoMainMenu["width"] = 10
-            self.botao.pack(side = LEFT)
+            self.botaoMainMenu.pack(side = LEFT)
             
             self.botaoCancel = Button(self.sextoContainer)
             self.botaoCancel["text"] = "CANCEL"
             self.botaoCancel["font"] = self.fontePadrao
             self.botaoCancel["command"] = self.eraseinput
             self.botaoCancel["width"] = 10
-            self.botao.pack(side = LEFT)
+            self.botaoCancel.pack(side = LEFT)
             
             self.botaoFingerprint = Button(self.sextoContainer)
             self.botaoFingerprint["text"] = "FINGERPRINT"
             self.botaoFingerprint["font"] = self.fontePadrao
-            self.botaoFingeprint["command"] = self.enabledb
+            self.botaoFingerprint["command"] = self.enabledb
             self.botaoFingerprint["width"] = 10
-            self.botao.pack(side = LEFT)
+            self.botaoFingerprint.pack(side = LEFT)
             
         def showinput(self):
             p_name = self.name.get()
             p_admin = self.var
             
-            if self.msg["text"] == "Name: \n Admin:" and p_admin == 1:
-                self.msg["text"] = "Name: " + p_name + "\n Admin: YES"
-                self.botaoFingerprint["state"] = DISABLED
+            if self.msg["text"] == "Name: \n Admin:":
+                if p_admin == 1:                
+                    self.msg["text"] = "Name: " + p_name + "\n Admin: YES"
+                    self.botaoFingerprint["state"] = DISABLED
             elif self.msg["text"] == "Name: \n Admin:" and p_admin == 0:
                 self.msg["text"] = "Name: " + p_name + "\n Aadmin: NO"
                 self.botaoFingerprint["state"] = DISABLED
             else:
                 self.msg["text"] = "Name: \n Admin:"
                 
-        def eraseinput(self);
+        def eraseinput(self):
             p_name = self.name.get()
             p_admin = self.var
             
@@ -144,7 +143,31 @@ def telaquatro()
                 conn.commit()
                 print('Dados inseridos com sucesso')
                 conn.close()
-                
+                fechar()
                 print('Abriu tela de Fingerprint')
             except:
-                
+                if self.msg["text"] == "Name: " + p_name + "\n Admin: YES" or self.msg["text"] == "Name: " + p_name + "\n Admin: NO":
+                    self.msg["text"] = "O Nome já está cadastrado. Insira novos dados!"
+                    self.botaoLoad["state"] = DISABLED
+                    self.botaoFingerprint["state"] = DISABLED
+                    self.name.delete(0,END)
+                    del(p_name)
+                    del(p_admin)
+                    self.botaoLoad["state"] = NORMAL
+                    self.botaoFingerprint["state"] = NORMAL
+                else:
+                    self.msg["text"] = "Name: \n Admin:"
+        
+    def returntohome():
+        fechar()
+        print("retornou ao menu inicial")
+    
+    def fechar():
+        root.destroy()
+
+    root = Tk()
+    ScreenFour(root)
+    root.title("Enroll Screen")
+    root.geometry('478x270')
+    #root.overrideredirect(True)
+    root.mainloop()
